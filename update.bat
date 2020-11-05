c74732bbef2cfb3f9d254a7c5f4c14d6652f9ad8
13531 <vziz@sina.com>
2020-11-06 02:58:21 +0800

    git快速提交

--- update.bat
+++ update.bat
@@ -1,54 +1,53 @@
 @echo off
-title GIT提交批处理——魏小敏
-echo *************************************************************
-echo *                GIT 代码提交批处理                         *
-echo *  Author: 魏小敏  13531@github                             *
-echo *  Version: 1.1                                             *
-echo *  Created: 2017/3/26                                       *
-echo *  Last Updated: 2020/11/5                                  *
-echo *  Last Updated by: 13531@github                            *
-echo *************************************************************
-
+title GIT快速提交工具
+echo 当前目录：%cd%
+goto start
+:seturl
+echo 请输入远程仓库地址:
+set /p url=
 ::查看git对应的远程仓库地址
-echo 上次使用的git对应的远程仓库地址
-echo -----------------------[git message]-------------------------
-git remote -v
-echo -------------------------------------------------------------
+::echo 上次使用的git对应的远程仓库地址
+::echo -----------------------[git message]-------------------------
+::git remote -v
+::echo -------------------------------------------------------------
 ::删除关联对应的远程仓库地址
-git remote rm origin 
+::git remote rm origin 
 ::查看是否删除成功，如果没有任何返回结果，表示OK
-git remote -v 
-::重新关联git远程仓库地址
-git remote add origin https://github.com/13531/test-git
-
-echo 当前git对应的远程仓库地址已修改为
-echo -----------------------[git message]-------------------------
+::git remote -v 
+::重新关联git远程仓库地址  例如https://github.com/13531/test-git
+::git remote add origin %url%
+git remote set-url origin %url%
+:start
+echo -------------------------------------------------------------
+echo 当前git对应的远程仓库地址:
 git remote -v 
 echo -------------------------------------------------------------
+echo 按键 [1] 进入下一步 
+echo.
+echo 按键 [9] 设置远程仓库地址. 
+echo.
+echo 按键 [C] 退出
 echo;
+choice /C 19c /n
+if %errorlevel%==2   goto seturl
+if %errorlevel%==3   goto end
+
 echo 开始提交代码到本地仓库
-echo 当前目录是：%cd%
-echo;
-pause
-echo;
-echo 当前git的版本
-echo -----------------------[git message]-------------------------
+
+:: 当前git的版本
 git --version
 echo -------------------------------------------------------------
-echo 开始添加变更
-echo -----------------------[git message]-------------------------
+::开始添加变更
 git add -A .
 echo -------------------------------------------------------------
-echo 执行结束！
-echo 提交变更到本地仓库
-@echo off
+
 :input
 set /p var=请输入提交说明:
 ::去除所有空格进行验证
 set "str=%var: =%"
 if "%str%"=="" goto input
 echo;
-echo 您输入的提交说明为：  %var%
+echo 提交说明为：  %var%
 echo -----------------------[git message]-------------------------
 git commit -m %var%
 echo -------------------------------------------------------------
@@ -57,6 +56,31 @@ echo -----------------------[git message]-------------------------
 git push origin master
 echo -------------------------------------------------------------
 echo 批处理执行完毕！
-
-
+echo;
+:menu
+echo 如果出现错误,
+echo.
+echo 按键 [1] 执行git pull origin master 尝试解决问题
+echo.
+echo 按键 [C] 退出
+echo;
+choice /C 1c /n
+if %errorlevel%==1   goto pull
+if %errorlevel%==2   goto end
+echo;
+goto end
+:pull
+echo -----------------[git pull origin master]--------------------
+git pull origin master
+echo -------------------------------------------------------------
+echo.
+echo 按键 [1] 进入下一步git push origin master 
+echo 按键 [C] 退出
+echo;
+choice /C 1c /n
+if %errorlevel%==2   goto end
+git push origin master
+echo -------------------------------------------------------------
+echo 批处理执行完毕！
 pause
+:end
